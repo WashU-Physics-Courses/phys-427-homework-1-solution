6442e6c400a76d785ce31dc730bb79b5b03aa3d6
fizban007 <fizban007@gmail.com>
2023-09-19 09:15:12 -0500

    removed the unnecessary function as well

--- problem3.cpp
+++ problem3.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <cmath>
 
-double next(double phi_n, double phi_nm1) {
-  return phi_nm1 - phi_n;
-}
-
 int main(int argc, char *argv[]) {
   double phi = 0.61803398;
 
@@ -12,7 +8,7 @@ int main(int argc, char *argv[]) {
   double phi1 = phi;
   std::cout << "phi^1: " << phi1 << std::endl;
   for (int i = 2; i <= 30; i++) {
-    double phi2 = next(phi1, phi0);
+    double phi2 = phi0 - phi1;
     std::cout << "phi^" << i << ": " << phi2 << " vs " << std::pow(phi, i) << std::endl;
     phi0 = phi1;
     phi1 = phi2;
